2ae5e65d552a98cb6065e91661aaa1cacb0689a7
duoshoumiao <94856184+duoshoumiao@users.noreply.github.com>
2026-05-14 09:34:41 +0800

    Update tools.py

--- autopcr/module/modules/tools.py
+++ autopcr/module/modules/tools.py
@@ -1137,7 +1137,7 @@ class get_normal_quest_recommand(Module):
 @description('从指定面板的指定队开始清除指定数量的编队')
 @inttype("clear_team_num", "队伍数", 1, [i for i in range(1, 21)])
 @inttype("clear_party_start_num", "初始队伍", 1, [i for i in range(1, 21)])
-@inttype("clear_tab_start_num", "初始面板", 1, [i for i in range(1, 7)])
+@inttype("clear_tab_start_num", "初始面板", 1, [i for i in range(1, 6)])
 @name('清除编队')
 class clear_my_party(Module):
     async def do_task(self, client: pcrclient):
